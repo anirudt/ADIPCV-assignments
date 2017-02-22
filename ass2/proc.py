e9473301f44245ce49d0a78367fdaab885797342
Anirud Thyagharajan <anirudt@gmail.com>
2017-02-22 16:18:52 +0530

    working proc

--- ass2/proc.py
+++ ass2/proc.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
+"""
+python proc.py
+"""
 import numpy as np
 from scipy.linalg import solve
 import os
 import cPickle as pickle
 import pdb
 import cv2
-import pdb
 
-def null(A, eps=1e-15):
+def null(A, eps=1e-7):
     """ Computes the null space of the matrix A """
     u, s, vh = np.linalg.svd(A)
     null_space = np.compress(s <= eps, vh, axis=0)
-    return null_space.T
+    return vh[:,-1]
 
 def solve(A):
     """ Finds the eigenvector corresponding to the smallest eigenvalue
@@ -27,11 +29,11 @@ def apply_homography(img, h):
         for j in xrange(ncols):
             x = np.array([i, j, 1])
             x_dash = np.dot(h, x)
-            x_dash = x_dash / (x_dash[3])
-            if np.any(x_dash < 0):
+            x_dash = x_dash / (x_dash[2])
+            if np.any(x_dash < 0) or (x_dash[0] >= nrows or x_dash[1] >= ncols):
                 continue
             else:
-                new_img[np.round(x_dash[0]), np.round(x_dash[1])] = img[i, j]
+                new_img[np.floor(x_dash[0])-1, np.floor(x_dash[1])-1] = img[i, j]
 
     return new_img
 
@@ -46,48 +48,87 @@ def compute_homography(X, X_dash):
     A = np.zeros((2*N, 9))
     #pdb.set_trace()
     for i in xrange(0,N):
-        x, y = X[i]
-        x_dash, y_dash = X_dash[i]
+        x, y = X[i][0]
+        x_dash, y_dash = X_dash[i][0]
         
         A[i*2,:] = np.array([-x, -y, -1, 0, 0, 0, x*x_dash, y*x_dash, x_dash])
         A[i*2+1,:] = np.array([0, 0, 0, -x, -y, -1, x*y_dash, y*y_dash, y_dash ])
 
-    h = solve(A)
+    h = null(A)
     
     h = np.reshape(h, (3,3))
+    h = h/h[2,2]
     return h
 
 def compute_sift_matches(f1, f2):
     """ Expects two images filenames as input, and computes the SIFT
     descriptor matches between the two images """
-    img1 = cv2.imread(f1, cv2.IMREAD_COLOR)
-    img2 = cv2.imread(f2, cv2.IMREAD_COLOR)
+    img1 = cv2.imread(f1, cv2.IMREAD_GRAYSCALE)
+    img2 = cv2.imread(f2, cv2.IMREAD_GRAYSCALE)
 
-    orb = cv2.ORB_create()
+    # Initiate SIFT detector
+    sift = cv2.SIFT()
 
-    kp1, des1 = orb.detectAndCompute(img1, None)
-    kp2, des2 = orb.detectAndCompute(img2, None)
+    # find the keypoints and descriptors with SIFT
+    kp1, des1 = sift.detectAndCompute(img1,None)
+    kp2, des2 = sift.detectAndCompute(img2,None)
 
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_L1)
+    # Match descriptors.
+    matches = bf.knnMatch(des1,des2, k=2)
+    # Sort them in the order of their distance.
+    good = []
+    for m,n in matches:
+        if m.distance < 0.75*n.distance:
+            good.append(m)
+    # Draw first 10 matches.
 
-    matches = bf.match(des1, des2)
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
-    matched_points1, matched_points2 = [], []
+    M, mask = cv2.findHomography(dst_pts, src_pts, cv2.RANSAC, 5.0)
+    # Use custom version
+    #dst_pts, src_pts = list(dst_pts), list(src_pts)
+    #M = compute_homography(dst_pts, src_pts)
 
-    for idx, match in enumerate(matches):
-        idx1, idx2 = match.trainIdx, match.queryIdx
-        matched_points1.append(list(kp1[idx1].pt))
-        matched_points2.append(list(kp2[idx2].pt))
+    (w, h) = img1.shape
+    out = cv2.warpPerspective(img2, M, (h, w))
+    #out = apply_homography(img2, M)
+    cv2.imwrite("out_sift.png", out)
 
-    h = compute_homography(matched_points1, matched_points2)
+    return out
 
+def scene_summarise():
+    files = ['imgs/Nov17.jpg', 'imgs/Dec12.jpg', 'imgs/Jan7.jpg', 'imgs/Jan12.jpg', 'imgs/Jan22.jpg']
+    # TODO: Add the manual running part and save the files
 
+    out0 = compute_sift_matches(files[4], files[0])
+    out1 = compute_sift_matches(files[4], files[1])
+    out2 = compute_sift_matches(files[4], files[2])
+    out3 = compute_sift_matches(files[4], files[3])
+    out4 = compute_sift_matches(files[4], files[4])
+    #cv2.imwrite("out/homo_manual{0}.png".format(i), out)
 
+    # Working on differences, and adding masks to avoid
+    # blackened regions
+    diff0 = (out1 - out0) * (out1 > 0) * (out0 > 0)
+    cv2.imwrite("out/diff0.png", diff0)
+    diff1 = (out2 - out1) * (out2 > 0) * (out1 > 0)
+    cv2.imwrite("out/diff1.png", diff1)
+    diff2 = (out3 - out2) * (out3 > 0) * (out2 > 0)
+    cv2.imwrite("out/diff2.png", diff2)
+    diff3 = (out4 - out3) * (out4 > 0) * (out3 > 0)
+    cv2.imwrite("out/diff3.png", diff3)
+
+    result = (diff0+diff1+diff2+diff3)
+    cv2.imwrite("out/res.png", result)
+
+def compute_vanishing_line():
 
 if __name__ == "__main__":
     # Add a menu driven program
     """
-
     print "Let's Start"
     os.system("python gui.py imgs/Dec12.jpg imgs/Jan12.jpg")
     with open('data', 'rb') as g:
@@ -95,4 +136,4 @@ if __name__ == "__main__":
     h = compute_homography(X, X_dash)
     print h
     """
-    compute_sift_matches("imgs/Dec12.jpg", "imgs/Jan7.jpg")
+    scene_summarise()
