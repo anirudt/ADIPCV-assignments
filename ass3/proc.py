863869be8caf7d6c515a4dbeee9b85cb4bef8626
Anirud Thyagharajan <anirudt@gmail.com>
2017-03-24 16:49:30 +0530

    all parts done!

--- ass3/proc.py
+++ ass3/proc.py
@@ -11,6 +11,10 @@ def null(A, eps=1e-7):
     u, s, vh = np.linalg.svd(A)
     null_space = np.compress(s <= eps, vh, axis=0)
     f = null_space.T
+    print "u: {}, s: {}, vh: {}".format(u.shape, s.shape, vh.shape)
+    print f.shape
+    if f.size == 0:
+        return np.array([0,0,0,0])
     return f[:,0]
 
 def cross_product(a, b):
@@ -42,13 +46,15 @@ def compute_sift_matches(f1, f2):
     matches = bf.knnMatch(des1,des2, k=2)
     # Sort them in the order of their distance.
     good = []
+    cnt = 0
     for m,n in matches:
-        if m.distance < 0.75*n.distance:
+        if m.distance < 0.8*n.distance and cnt < 10:
             good.append(m)
+            cnt += 1
     # Draw first 10 matches.
 
-    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
-    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+    src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ])
+    dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ])
 
     return src_pts, dst_pts
 
@@ -56,13 +62,40 @@ def compute_fundamental(X, X_dash):
     """ Computes the fundamental matrix """
     N = len(X)
     A = np.zeros((N, 9))
+
+    # TODO: normalization!
+    T, T_dash = np.zeros((3,3)), np.zeros((3,3))
+
+    mu_x1 = np.mean(X[:, 0])
+    mu_y1 = np.mean(X[:, 1])
+    std_x1 = np.std(X[:, 0])
+    std_y1 = np.std(X[:, 1])
+
+    mu_x2 = np.mean(X_dash[:, 0])
+    mu_y2 = np.mean(X_dash[:, 1])
+    std_x2 = np.std(X_dash[:, 0])
+    std_y2 = np.std(X_dash[:, 1])
+
+    Tp = np.array([[1/std_x1, 0, -mu_x1/std_x1],
+                  [0, 1/std_y1, -mu_y1/std_y1],
+                  [0, 0, 1]])
+
+    Tp_dash = np.array([[1/std_x2, 0, -mu_x2/std_x2],
+                  [0, 1/std_y2, -mu_y2/std_y2],
+                  [0, 0, 1]])
+
     for i in xrange(N):
-        x, y = X[i][0]
-        x_dash, y_dash = X_dash[i][0]
+        x, y = X[i, :]
+        x_dash, y_dash = X_dash[i, :]
+        x, y = (x - mu_x1)/std_x1, (y - mu_y1)/std_y1
+        x_dash, y_dash = (x_dash - mu_x2)/std_x2, (y_dash - mu_y2)/std_y2
         A[i,:] = np.array([x_dash*x, x_dash*y, x_dash, y_dash*x, y_dash*y, y_dash, x_dash, y_dash, 1])
 
-    f = null(A)
-    F_loose = np.reshape(f, (3,3))
+    print A
+    u, s, vh = np.linalg.svd(A)
+    print vh
+    F_loose = vh[8, :].reshape((3,3))
+    print F_loose
 
     U, s, V = np.linalg.svd(F_loose)
 
@@ -70,9 +103,15 @@ def compute_fundamental(X, X_dash):
     s[2] = 0
     S = np.diag(s)
     F = np.dot(U, np.dot(S, V))
-    F = F/np.linalg.norm(F)
+    #F = F/np.linalg.norm(F)
+    print Tp, Tp_dash
+    print F
+    print "Net Dot product is {}".format(np.dot(A, F.ravel()).sum())
+    F = np.dot(Tp_dash.T, np.dot(F, Tp))
     print "Net Dot product is {}".format(np.dot(A, F.ravel()).sum())
     f, _ = cv2.findFundamentalMat(X, X_dash)
+
+    print "Fundamental matrices are {} and {}".format(f, F)
     print "Net Dot product_ is {}".format(np.dot(A, f.ravel()).sum())
 
     print "Determinants {} and {}".format(np.linalg.det(F), np.linalg.det(f))
@@ -95,10 +134,11 @@ def draw_epipolar_lines(X, X_dash, F):
 
     e, e_dash = compute_epipoles(F)
     e, e_dash = e/e[2], e_dash/e_dash[2]
+    print "epipoles are {} and {}".format(e, e_dash)
 
     line_selection = []
     for idx in xrange(N):
-        pt = np.array(list(X[idx][0])+[1])
+        pt = np.array(list(X[idx, :])+[1])
         line = np.dot(F, pt)
         R, C, ch = img2.shape
         a, b, c = line
@@ -130,7 +170,7 @@ def draw_epipolar_lines(X, X_dash, F):
 
     line_selection = []
     for idx in xrange(N):
-        pt = np.array(list(X_dash[idx][0])+[1])
+        pt = np.array(list(X_dash[idx, :])+[1])
         line = np.dot(F.T, pt)
         R, C, ch = img1.shape
         a, b, c = line
@@ -159,7 +199,7 @@ def draw_epipolar_lines(X, X_dash, F):
 
     e_calc = cross_product(line_selection[0], line_selection[1])
     e_calc /= e_calc[2]
-    print e - e_calc
+    print "diff: ", e - e_calc
     print e_dash - e_dash_calc
     cv2.imwrite("lines_1.png", img1)
     cv2.imwrite("lines_2.png", img2)
@@ -183,20 +223,44 @@ def linear_triangulation(X, X_dash, P, P_dash):
     """ Maps a linear triangulation to give a list of corresponding
     scene points """
     N = len(X)
-    scene_points = np.zeros((N, 4))
+    scene_points = []
     for idx in xrange(N):
-        x, y = X[idx][0]
-        x_dash, y_dash = X_dash[idx][0]
+        x, y = X[idx, :]
+        x_dash, y_dash = X_dash[idx, :]
         A1 = np.array(x * P[2, :] - P[0, :])
         A2 = np.array(y * P[2, :] - P[1, :])
         A3 = np.array(x_dash * P_dash[2, :] - P_dash[0, :])
         A4 = np.array(y_dash * P_dash[2, :] - P_dash[1, :])
-        A = np.array((A1, A2, A3, A4), axis=0)
+        A = np.array([A1, A2, A3, A4])
 
         scene_pt = null(A)
-        scene_points[i, :] = np.reshape(scene_pt, (1, 4))
+        if np.sum(scene_pt) == 0:
+            continue
+        scene_pt = scene_pt / scene_pt[3]
+        scene_points.append(np.reshape(scene_pt, (1,4)))
+    scene_points = np.vstack(scene_points)
 
     return scene_points
+
+def get_depths(X, X_dash, P, P_dash):
+    """ Get the depths of the scene points """
+    scene_points = linear_triangulation(X, X_dash, P, P_dash)
+    c1 = np.array([0,0,0,1])
+    c2 = np.append(-np.dot(np.linalg.inv(P[:3, :3]), P[:, 3]), [1])
+
+    diff_c1 = scene_points - c1
+    diff_c2 = scene_points - c2
+
+    princ_axis_1 = np.append(P[2,:3], [0])
+    princ_axis_2 = np.append(P_dash[2,:3], [0])
+    # Computing the dot products
+    depth_c1 = np.dot(diff_c1, princ_axis_1)
+    depth_c2 = np.dot(diff_c2, princ_axis_2)
+
+    depth_c1 = np.linalg.det(P[:3,:3]) * depth_c1
+    depth_c2 = np.linalg.det(P[:3,:3]) * depth_c2
+
+    return depth_c1, depth_c2
 
 def main():
     """ Main function """
@@ -205,7 +269,9 @@ def main():
     f, _ = cv2.findFundamentalMat(X, X_dash)
     draw_epipolar_lines(X, X_dash, f)
     e, e_dash = compute_epipoles(f)
-    P, P_dash = compute_projective_mats(F, e, e_dash)
+    P, P_dash = compute_projective_mats(f, e, e_dash)
+    depth_c1, depth_c2 = get_depths(X, X_dash, P, P_dash)
+    print depth_c1, depth_c2
 
 if __name__ == '__main__':
     main()
